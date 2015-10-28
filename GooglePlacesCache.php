8f216f82398cac442561f867464337085031b9d8
Ike Hecht <tosfos@yahoo.com>
2015-10-28 14:16:27 -0700

    Switch cache to use internal ObjectCache
    * deprecate GooglePlaces table
    
    Change-Id: Ic51b5c223d5bc0779a47e3cc90b612d6c3688404

--- GooglePlacesCache.php
+++ GooglePlacesCache.php
@@ -1,55 +1,44 @@
 <?php
 
 /**
- * Custom db cache for Google Places calls
+ * Cache for Google Places calls
  *
  * @author Ike Hecht
  */
 class GooglePlacesCache {
-	const TABLE = 'GooglePlaces';
 
 	/**
 	 * Get this call from db cache
 	 *
-	 * @param string $request
+	 * @param string $APIKey
+	 * @param string $placeID
 	 * @return string|boolean
 	 */
-	public static function getCache( $request ) {
-		$dbr = wfGetDB( DB_SLAVE );
-		/** @todo Is this platform independent? */
-		$conds = array( 'request' => md5( serialize( $request ) ), $dbr->encodeExpiry( wfTimestampNow() ) . ' < expiration' );
-		$result = $dbr->select( self::TABLE, 'response', $conds, __METHOD__ );
-
-		$row = $result->fetchObject();
-		if ( $row ) {
-			return ( unserialize( $row->response ) );
-		} else {
-			return false;
-		}
+	public static function getCache( $APIKey, $placeID ) {
+		$cache = wfGetCache( CACHE_ANYTHING );
+		$key = wfMemcKey( 'googleplaces', $APIKey, $placeID );
+		$cached = $cache->get( $key );
+		wfDebugLog( "GooglePlaces",
+			__METHOD__ . ": got " . var_export( $cached, true ) .
+			" from cache." );
+		return $cached;
 	}
 
 	/**
 	 * Store this call in cache
 	 *
-	 * @param string $request
+	 * @param string $APIKey
+	 * @param string $placeID
 	 * @param string $response
 	 * @param integer $cache_expire
 	 * @return boolean
-	 * @throws MWException
 	 */
-	public static function setCache( $request, $response, $cache_expire ) {
-		/** @todo: cleanup expired cache rows */
-		$dbw = wfGetDB( DB_MASTER );
-		$data = array(
-			'request' => md5( serialize( $request ) ),
-			'response' => serialize( $response ),
-			'expiration' => $dbw->encodeExpiry( wfTimestamp( TS_MW, time() + $cache_expire ) )
-		);
-		$result = $dbw->upsert( self::TABLE, $data, array( 'request' ), $data, __METHOD__ );
-		if ( !$result ) {
-			throw new MWException( 'Set Cache failed' );
-		}
-
-		return $result;
+	public static function setCache( $APIKey, $placeID, $response, $cache_expire = 0 ) {
+		$cache = wfGetCache( CACHE_ANYTHING );
+		$key = wfMemcKey( 'googleplaces', $APIKey, $placeID );
+		wfDebugLog( "GooglePlaces",
+			__METHOD__ . ": caching " . var_export( $response, true ) .
+			" from Google." );
+		return $cache->set( $key, $response, $cache_expire );
 	}
 }
